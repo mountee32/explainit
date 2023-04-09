5478d404514debbcf8fcc97b06d208a4cd0789dc
Andy <mountee@msn.com>
2023-04-09 11:48:23 +0100

    trying api fix

--- api/config.php
+++ api/config.php
@@ -4,6 +4,12 @@ $db_name = 'u383132761_explaint';
 $username = 'u383132761_GG78';
 $password = 'Jump6565%True';
 
+// Set the response headers
+header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
 try {
     $conn = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
