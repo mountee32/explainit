e0d6a2c38828281e8c3cc3a9cebac26b54486a27
Andy <mountee@msn.com>
2023-04-11 13:44:33 +0100

    config.php updates

--- api/config.php
+++ api/config.php
@@ -3,7 +3,9 @@ $host = 'localhost';
 $db_name = 'u383132761_explaint';
 $username = 'u383132761_GG78';
 $password = 'Jump6565%True';
-
+$log_file = 'api-log.txt';
+date_default_timezone_set('UTC');
+$time_stamp = date('Y-m-d H:i:s');
 // Set the response headers
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
