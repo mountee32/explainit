8873666b6f43299c4beed7e67a36e7e9baaa21e3
Andy <mountee@msn.com>
2023-04-23 21:50:50 +0100

    incredible gpt api creating articles

--- python/WPimportArticles.py
+++ python/WPimportArticles.py
@@ -2,6 +2,7 @@ import os
 import base64
 import requests
 import pandas as pd
+import openai
 from dotenv import load_dotenv
 from html import unescape
 
@@ -13,55 +14,114 @@ username = os.getenv("WORDPRESS_USERNAME")
 password = os.getenv("WORDPRESS_APP_PASSWORD")
 website = os.getenv("WORDPRESS_URL")
 csv_file = 'csv/uploadposts.csv'
+openai_api_key = os.getenv("OPENAI_API_KEY")
 
 headers = {
     "Authorization": f"Basic {base64.b64encode(f'{username}:{password}'.encode()).decode()}",
 }
 
+openai.api_key = openai_api_key
+
 def list_current_questions():
     response = requests.get(f"{website}/wp-json/wp/v2/posts?categories=1&per_page=100", headers=headers)
     posts = response.json()
-    print("Current questions:")
-    for post in posts:
-        print(unescape(post['title']['rendered']))
-    
-    print("\nGPT-4 prompt:")
     existing_titles = [unescape(post['title']['rendered']) for post in posts]
-    prompt = f"Using your knowledge and understanding of Christianity, please identify 5 additional popular questions that are often asked of Christians about their faith and worldview that are not already covered in the following list:\n\n"
+    return existing_titles
+
+def gpt_generate_questions(existing_titles):
+    prompt = f"Using your knowledge and understanding of Christianity as a Christian theologian and teacher, please identify 5 additional popular questions that are often asked of Christians about their faith and worldview that are not already covered in the following list:\n\n"
     prompt += "\n".join(existing_titles)
-    prompt += "\n\nFor each of the 5 questions, please write an article in CSV format that is compatible with the uploader provided. Each article should be between 250 and 500 words. Use the following format for each article: title, content, tags, category\n"
-    print(prompt)
+    prompt += "\n\nPlease list the 5 questions."
 
-def import_csv_posts():
-    # Read the CSV file
-    df = pd.read_csv(csv_file)
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        max_tokens=150,
+        n=1,
+        stop=None,
+        temperature=0.8,
+    )
 
-    # Loop through each row in the CSV file and post the article
-    for index, row in df.iterrows():
-        title = row['title']
-        content = row['content']
+    generated_questions = response.choices[0].text.strip().split("\n")
+    return generated_questions
+
+def request_and_confirm_questions():
+    existing_titles = list_current_questions()
+    while True:
+        generated_questions = gpt_generate_questions(existing_titles)
+        print("\nSuggested questions:")
+        for i, question in enumerate(generated_questions):
+            print(f"{i + 1}. {question}")
+
+        action = input("\nEnter 'Yes' to create articles for these questions, 'Retry' to generate new questions, or 'Cancel' to return to the main menu: ")
+
+        if action.lower() == 'yes':
+            return generated_questions
+        elif action.lower() == 'retry':
+            continue
+        elif action.lower() == 'cancel':
+            return None
+        else:
+            print("Invalid input. Please enter 'Yes', 'Retry', or 'Cancel'.")
+def post_article(title, content, category):
+    headers = {
+        "Authorization": f"Basic {base64.b64encode(f'{username}:{password}'.encode()).decode()}",
+    }
+    data = {
+        "title": title,
+        "content": content,
+        "status": "publish",
+        "categories": category,
+    }
+    response = requests.post(f"{website}/wp-json/wp/v2/posts", headers=headers, json=data)
+    return response.json()
+
+def create_and_upload_articles(questions):
+    for question in questions:
+        # Generate the article content
+        prompt = f"Please write an article about the following question related to Christianity: '{question}'. The article should be between 250 and 500 words."
+
+        response = openai.Completion.create(
+            engine="text-davinci-002",
+            prompt=prompt,
+            max_tokens=800,
+            n=1,
+            stop=None,
+            temperature=0.8,
+        )
+
+        content = response.choices[0].text.strip()
+
+        # Upload the article to the WordPress site
         category = "1"
-
-        response = post_article(title, content, category)
+        response = post_article(question, content, category)
 
         if 'id' in response:
-            print(f"Successfully posted article '{title}' with ID {response['id']}.")
+            print(f"Successfully posted article '{question}' with ID {response['id']}.")
         else:
-            print(f"Error posting article '{title}': {response['code']} - {response['message']}")
+            print(f"Error posting article '{question}': {response['code']} - {response['message']}")
 
 while True:
     print("\nMenu:")
     print("1. List current questions")
     print("2. Import CSV Posts")
     print("3. Exit")
-    choice = input("Enter your choice (1-3): ")
+    print("4. Request GPT-4 to generate and upload 5 new questions")
+    choice = input("Enter your choice (1-4): ")
 
     if choice == '1':
-        list_current_questions()
+        existing_titles = list_current_questions()
+        print("\nCurrent questions:")
+        for title in existing_titles:
+            print(title)
     elif choice == '2':
         import_csv_posts()
     elif choice == '3':
         print("Exiting...")
         break
+    elif choice == '4':
+        questions_to_create = request_and_confirm_questions()
+        if questions_to_create is not None:
+            create_and_upload_articles(questions_to_create)
     else:
-        print("Invalid choice. Please enter a number between 1 and 3.")
+        print("Invalid choice. Please enter a number between 1 and 4.")
