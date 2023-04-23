31d85f35a114f97e098fc3a1d35320b8e0e17f30
Andy <mountee@msn.com>
2023-04-24 00:37:27 +0100

    wordpress tages menu 6 not working

--- python/WPimportArticles.py
+++ python/WPimportArticles.py
@@ -5,6 +5,7 @@ import pandas as pd
 import openai
 from dotenv import load_dotenv
 from html import unescape
+import os
 
 # Load environment variables from .env file
 load_dotenv()
@@ -26,6 +27,16 @@ headers = {
 
 openai.api_key = openai_api_key
 
+def get_existing_tags():
+    response = requests.get(f"{website}/wp-json/wp/v2/tags?per_page=100", headers=headers)
+    tags = response.json()
+    existing_tags = {tag['name']: tag['id'] for tag in tags}
+    return existing_tags
+
+
+
+def clear_console():
+    os.system('cls' if os.name == 'nt' else 'clear')
 def list_current_questions():
     response = requests.get(f"{website}/wp-json/wp/v2/posts?categories=1&per_page=100", headers=headers)
     posts = response.json()
@@ -109,14 +120,117 @@ def create_and_upload_articles(questions):
             print(f"Successfully posted article '{question}' with ID {response['id']}.")
         else:
             print(f"Error posting article '{question}': {response['code']} - {response['message']}")
+def list_tags_per_post():
+    response = requests.get(f"{website}/wp-json/wp/v2/posts?_embed&per_page=100", headers=headers)
+    posts = response.json()
+    for post in posts:
+        title = unescape(post['title']['rendered'])
+        tags = [tag['name'] for tag in post['_embedded']['wp:term'][1]]
+        print(f"{title} - Tags: {', '.join(tags)}")
+
+def get_best_matching_tags(post_titles, existing_tags):
+    prompt = f"Given the following post titles and tags:\n\nTitles:\n"
+    prompt += "\n".join(post_titles)
+    prompt += f"\n\nTags:\n{', '.join(existing_tags)}\n\nPlease provide the best matching tags for each post title, where a post can have multiple relevant tags. Do NOT include the post title as a tag. Separate tags for each post with commas and put each title with its tags on a new line."
+
+    response = openai.Completion.create(
+        engine=myengine,
+        prompt=prompt,
+        max_tokens=int(mytokens),
+        n=1,
+        stop=None,
+        temperature=0.5,  # Changed temperature to 0.5
+    )
+
+    matching_tags = response.choices[0].text.strip().split("\n")
+    matching_tags = [
+        ", ".join([
+            tag.strip() for tag in tags.split(",") 
+            if tag.strip() not in post_titles and not any(title in tag for title in post_titles)
+        ]) for tags in matching_tags
+    ]
+    return matching_tags
+
+def create_tag_if_not_exists(tag_name):
+    headers = {
+        "Authorization": f"Basic {base64.b64encode(f'{username}:{password}'.encode()).decode()}",
+    }
+    data = {
+        "name": tag_name,
+    }
+    response = requests.post(f"{website}/wp-json/wp/v2/tags", headers=headers, json=data)
+    if response.status_code == 201:
+        print(f"Successfully created tag '{tag_name}' with ID {response.json()['id']}.")
+        return response.json()['id']
+    else:
+        print(f"Error creating tag '{tag_name}': {response.status_code} - {response.text}")
+        return None
+
+
+def update_post_tags(post_id, tag_ids):
+    headers = {
+        "Authorization": f"Basic {base64.b64encode(f'{username}:{password}'.encode()).decode()}",
+    }
+    data = {
+        "tags": tag_ids,
+    }
+    response = requests.post(f"{website}/wp-json/wp/v2/posts/{post_id}", headers=headers, json=data)
+    if response.status_code == 200:
+        print(f"Successfully updated tags for post ID {post_id}.")
+    else:
+        print(f"Error updating tags for post ID {post_id}: {response.status_code} - {response.text}")
+    return response.json()
+
+def manage_tags_for_posts():
+    existing_titles = list_current_questions()
+    existing_tags = get_existing_tags()
+
+    while True:
+        suggested_tags = get_best_matching_tags(existing_titles, existing_tags)
+
+        if len(suggested_tags) != len(existing_titles):
+            print("There was an issue retrieving the suggested tags. Retrying...")
+            continue
+
+        print("\nSuggested tags for posts:")
+        for i, suggested_tag in enumerate(suggested_tags):
+            print(f"{existing_titles[i]} - Tags: {suggested_tag}")
+
+        action = input("\nEnter 'Accept' to update tags, 'Retry' to generate new suggestions, or 'Cancel' to return to the main menu: ")
+
+        if action.lower() == 'accept':
+            response = requests.get(f"{website}/wp-json/wp/v2/posts?_embed&per_page=100", headers=headers)
+            posts = response.json()
+            existing_tags = get_existing_tags()
+            for post, suggested_tag in zip(posts, suggested_tags):
+                post_id = post['id']
+                tag_ids = [
+                    existing_tags[tag.strip()] if tag.strip() in existing_tags else create_tag_if_not_exists(tag.strip())
+                    for tag in suggested_tag.split(', ')
+                ]
+                update_post_tags(post_id, tag_ids)  # Pass post_id and tag_ids to update_post_tags
+                        # Wait for a key press before returning to the main menu
+            input("Press any key to return to the main menu...")
+            break
+        elif action.lower() == 'retry':
+            continue
+        elif action.lower() == 'cancel':
+            break
+        else:
+            print("Invalid input. Please enter 'Accept', 'Retry', or 'Cancel'.")
+
 
 while True:
+    clear_console()  # Add this line to clear the console before displaying the menu
+    print("\nMenu:")
     print("\nMenu:")
     print("1. List current questions")
     print("2. Import CSV Posts")
     print("3. Exit")
     print("4. Request GPT-3 to generate and upload 5 new questions")
-    choice = input("Enter your choice (1-4): ")
+    print("5. List WordPress tags per post")
+    print("6. Get best matching tags for posts and update")  # Added menu option 6
+    choice = input("Enter your choice (1-6): ")
 
     if choice == '1':
         existing_titles = list_current_questions()
@@ -132,5 +246,9 @@ while True:
         questions_to_create = request_and_confirm_questions()
         if questions_to_create is not None:
             create_and_upload_articles(questions_to_create)
+    elif choice == '5':
+        list_tags_per_post()
+    elif choice == '6':  # Added choice for the new menu option 6
+        manage_tags_for_posts()
     else:
-        print("Invalid choice. Please enter a number between 1 and 4.")
+        print("Invalid choice. Please enter a number between 1 and 6.")
