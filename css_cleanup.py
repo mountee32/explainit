fe7e5b7da9a49d8a777b63ccd5d52f1e532cadac
Andy <mountee@msn.com>
2023-05-08 17:43:39 +0100

    styles cleanup

--- css_cleanup.py
+++ css_cleanup.py
@@ -16,21 +16,22 @@ def read_file_contents(path):
 def parse_css(entry_path):
     content = read_file_contents(entry_path)
     if content:
-        pattern = r'(?P<selector>[^{]+)\s*{\s*(?P<properties>[^}]+)}'
-        matches = re.findall(pattern, content, re.MULTILINE)
-        style_defs = {}
-        for match in matches:
-            selector = match[0].strip()
-            properties = match[1].strip()
-            style_defs[selector] = properties
-        return style_defs
-    return {}
+        # Remove CSS comments
+        content = re.sub(r'/\*.*?\*/', '', content, flags=re.DOTALL)
 
+        # Updated pattern to exclude at-rules and match only valid style names
+        pattern = r'(?P<selector>[^@{}]+)\s*{\s*(?P<properties>[^}]+)}'
+        matches = re.finditer(pattern, content, re.MULTILINE)
+
+        valid_selector = re.compile(r'^[.#]?[a-zA-Z][a-zA-Z0-9_-]*')
+        style_data = [(match.group('selector').strip(), content[:match.start()].count('\n') + 1) for match in matches if valid_selector.match(match.group('selector').strip())]
+        return style_data
+    return []
 def parse_file(entry_path, styles_defined):
     if entry_path.endswith('.css'):
-        style_defs = parse_css(entry_path)
-        if style_defs:
-            styles_defined[entry_path] = style_defs
+        style_names = parse_css(entry_path)
+        if style_names:
+            styles_defined[entry_path] = style_names
 
 def traverse_and_parse_contents(current_path, styles_defined):
     for entry in os.listdir(current_path):
@@ -43,23 +44,64 @@ def traverse_and_parse_contents(current_path, styles_defined):
 def export_styles_defined_to_csv(filename, styles_defined):
     with open(filename, 'w', newline='', encoding='utf-8') as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
-        writer.writerow(['File', 'Style', 'Properties'])
-        for file, style_defs in styles_defined.items():
-            for selector, properties in style_defs.items():
-                writer.writerow([file, selector, properties])
+        writer.writerow(['File', 'Style', 'Line'])
+        for file, style_data in styles_defined.items():
+            for selector, line in style_data:
+                writer.writerow([file, selector, line])
+def sort_css_styles(file_path):
+    style_data = parse_css(file_path)
+
+    # Sort style_data alphabetically based on the style selector
+    style_data.sort(key=lambda x: x[0])
+
+    content = read_file_contents(file_path)
+    # Remove CSS comments
+    content = re.sub(r'/\*.*?\*/', '', content, flags=re.DOTALL)
+
+    sorted_content = ""
+    for style, line_number in style_data:
+        match = re.search(rf"{style}\s*{{[^}}]*}}", content, re.MULTILINE)
+        if match:
+            sorted_content += f"{match.group()}\n\n"
+
+    with open("styles_sorted.css", "w", encoding="utf-8") as sorted_file:
+        sorted_file.write(sorted_content)
 
 def main():
-    parser = argparse.ArgumentParser(description="CSS cleanup utility - Iteration 2")
-    parser.add_argument("-p", "--path", type=str, default=os.getcwd(), help="Path of the project directory")
-    args = parser.parse_args()
+    while True:
+        print("\nOptions:")
+        print("1. Create CSV with style names")
+        print("2. Sort CSS styles alphabetically and save to styles_sorted.css")
+        print("3. Exit")
+        choice = input("\nEnter your choice (1, 2, or 3): ")
 
-    current_folder = args.path
+        if choice == "1":
+            styles_defined = dict()
+            for root, _, files in os.walk("."):
+                for file in files:
+                    if file.endswith(".css"):
+                        entry_path = os.path.join(root, file)
+                        style_data = parse_css(entry_path)
+                        if style_data:
+                            styles_defined[entry_path] = style_data
 
-    styles_defined = {}
+            export_styles_defined_to_csv("styles.csv", styles_defined)
+            print("\nCSV file created: styles.csv")
 
-    traverse_and_parse_contents(current_folder, styles_defined)
+        elif choice == "2":
+            file_path = input("\nEnter the CSS file path to be sorted: ")
+            if os.path.isfile(file_path) and file_path.endswith(".css"):
+                sort_css_styles(file_path)
+                print("\nSorted CSS styles saved to styles_sorted.css")
+            else:
+                print("\nInvalid file path. Please provide a valid CSS file path.")
 
-    export_styles_defined_to_csv('styles-defined.csv', styles_defined)
+        elif choice == "3":
+            print("\nGoodbye!")
+            break
+
+        else:
+            print("\nInvalid choice. Please enter 1, 2, or 3.")
 
 if __name__ == "__main__":
     main()
