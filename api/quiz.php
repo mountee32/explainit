e32ad67fc32ca6ec2f0841b1615a750429b0cfcb
Andy <mountee@msn.com>
2023-05-31 15:50:51 +0100

    quiz

--- api/quiz.php
+++ api/quiz.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // hosted https://ai4christians.com/api/quiz.php
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
