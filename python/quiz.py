408c347ab5f1a5a24cf6850c37713050e2ee905f
Andy <mountee@msn.com>
2023-05-31 18:16:45 +0100

    quiz py not working

--- python/quiz.py
+++ python/quiz.py
@@ -1,5 +1,6 @@
 import requests
 import pandas as pd
+from chatbot import askgpt
 
 API_URL = "https://ai4christians.com/api/quiz.php"
 
@@ -21,8 +22,6 @@ def delete_question(id):
 
 def create_question(question_data):
     # Handle 'nan' values
-    if pd.isna(question_data['date_reviewed']):
-        question_data['date_reviewed'] = ''
     if pd.isna(question_data['category']):
         question_data['category'] = ''
     if pd.isna(question_data['status']):
@@ -42,7 +41,6 @@ def create_question(question_data):
         print(f"Timeout Error: {errt}")
     except requests.exceptions.RequestException as err:
         print(f"Unexpected Error: {err}")
-
 
 def print_questions(questions):
     df = pd.DataFrame(questions)
@@ -69,7 +67,53 @@ def menu():
     print("2. Export all questions to CSV")
     print("3. Delete all questions")
     print("4. Import all questions from CSV")
-    print("5. Exit")
+    print("5. Create new quiz questions with OpenAI")
+    print("6. Exit")
+
+def generate_questions(category):
+    questions = []
+    for i in range(2):
+        question_data = {
+            "action": "create",
+            "question": {},
+            "skill": "hard",
+            "choice1": "",
+            "choice2": "",
+            "choice3": "",
+            "choice4": "",
+            "correct_choice": 0,
+            "explanation1": "",
+            "explanation2": "",
+            "explanation3": "",
+            "explanation4": "",
+            "category": category,
+            "status": "drafted"
+        }
+
+        question_prompt = f"ChatGPT, please generate a quiz question related to the category {category} in JSON format with the following fields: `question`, 'skill', `choice1`, `choice2`, `choice3`, `choice4`, `correct_choice numbered 1 to 4`, `explanation1`, `explanation2`, `explanation3`, `explanation4`, 'category', 'status'. Please set the 'status' to draft, the 'category' to {category}, and the 'skill' to medium."
+        print(question_prompt)
+        answer, chat_log = askgpt(question_prompt)
+        print("\n")
+        print(answer)
+        question_data["question"] = answer
+
+        # Send the question data to the API
+        try:
+            response = requests.post(API_URL, json=question_data, headers={"Content-Type": "application/json"})
+            print("API Response Status Code:", response.status_code)
+            print("API Response Headers:", response.headers)
+            response.raise_for_status()
+
+            # Check if the response contains valid JSON
+            try:
+                question = response.json()
+                questions.append(question)
+            except ValueError:
+                print("Invalid JSON response:", response.text)
+        except requests.exceptions.RequestException as err:
+            print(f"Error creating question: {err}")
+
+    return questions
 
 def main():
     while True:
@@ -98,9 +142,27 @@ def main():
                     create_question(question)
                 print("All questions imported.")
         elif choice == "5":
+            print("Select a category to generate questions for:")
+            print("1. Christianity History")
+            print("2. Bible Books")
+            print("3. Bible Characters")
+            category_choice = input("Choose a category: ")
+            categories = {
+                "1": "Christianity History",
+                "2": "Bible Books",
+                "3": "Bible Characters",
+            }
+            if category_choice in categories:
+                questions = generate_questions(categories[category_choice])
+                for question in questions:
+                    create_question(question)
+                print("Quiz questions created and uploaded.")
+            else:
+                print("Invalid category choice.")
+        elif choice == "6":
             break
         else:
-            print("Invalid choice. Please enter a number from 1 to 5.")
+            print("Invalid choice. Please enter a number from 1 to 6.")
 
 if __name__ == "__main__":
     main()
