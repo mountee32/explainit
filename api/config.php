d7a222b848da0a2a0b34f9cf0c79a32207995358
Andy <mountee@msn.com>
2023-06-02 22:24:34 +0100

    fixing config.php

--- api/config.php
+++ api/config.php
@@ -7,7 +7,7 @@ $password = $env['DB_PASSWORD'];
 $log_file = 'api-log.txt';
 date_default_timezone_set('UTC');
 $time_stamp = date('Y-m-d H:i:s');
-// Set the response headers - updated
+
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
@@ -17,15 +17,14 @@ try {
     $conn = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $exception) {
-    echo "Connection error: " . $exception->getMessage();
+    $errorMessage = "Connection error: " . $exception->getMessage();
+    echo $errorMessage;
+    file_put_contents($log_file, "{$time_stamp} config.php - {$errorMessage}\n", FILE_APPEND);
 }
-
 
 if (!is_null($conn) && $conn instanceof PDO) {
-    // Connection is valid
     file_put_contents($log_file, "{$time_stamp} config.php - Database connection valid.\n", FILE_APPEND);
-  
 } else {
-    // Connection is invalid
     file_put_contents($log_file, "{$time_stamp} config.php - Database connection invalid.\n", FILE_APPEND);
 }
+?>
